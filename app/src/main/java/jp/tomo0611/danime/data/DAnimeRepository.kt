4b14f8ed305e99278a89a8215effa7d74b3f1757
tomo0611 <tomo0611@hotmail.com>
2024-07-18 02:32:54 +0900

    EpisodeAPIを追加

--- app/src/main/java/jp/tomo0611/danime/data/DAnimeRepository.kt
+++ app/src/main/java/jp/tomo0611/danime/data/DAnimeRepository.kt
@@ -1,14 +1,44 @@
 package jp.tomo0611.danime.data
 
+import jp.tomo0611.danime.model.GetEpisodesRequest
+import jp.tomo0611.danime.model.GetEpisodesResponse
 import jp.tomo0611.danime.model.GetRecentlyAiredEpisodesResponse
+import jp.tomo0611.danime.model.GetTitleDetailRequest
+import jp.tomo0611.danime.model.GetTitleDetailResponse
+import jp.tomo0611.danime.model.GetTitleParams
+import jp.tomo0611.danime.model.Params
 import jp.tomo0611.danime.network.DAnimeApiService
 
 interface DAnimeRepository {
     suspend fun getRecentlyAiredEpisodes(): GetRecentlyAiredEpisodesResponse
+    suspend fun getTitleDetail(workId: String): GetTitleDetailResponse
+    suspend fun getEpisodes(workId: String): GetEpisodesResponse
 }
 
 class NetworkDAnimeRepository(
     private val dAnimeApiService: DAnimeApiService
 ) : DAnimeRepository {
-    override suspend fun getRecentlyAiredEpisodes(): GetRecentlyAiredEpisodesResponse = dAnimeApiService.getRecentlyAiredEpisodes()
+    override suspend fun getRecentlyAiredEpisodes(): GetRecentlyAiredEpisodesResponse =
+        dAnimeApiService.getRecentlyAiredEpisodes()
+
+    override suspend fun getTitleDetail(
+        workId: String
+    ): GetTitleDetailResponse = dAnimeApiService.getTitleDetail(
+        listOf(
+            GetTitleDetailRequest(
+                jsonrpc = "2.0", id = "0000", method = "title",
+                params = GetTitleParams(videoTitleId = workId)
+            )
+        )
+    )[0]
+
+    override suspend fun getEpisodes(workId: String): GetEpisodesResponse =
+        dAnimeApiService.getEpisodes(
+            listOf(
+                GetEpisodesRequest(
+                    jsonrpc = "2.0", id = "0000", method = "episodeList",
+                    params = Params(videoTitleId = workId, sortType = 4)
+                )
+            )
+        )[0]
 }
--- app/src/main/java/jp/tomo0611/danime/network/DAnimeApiService.kt
+++ app/src/main/java/jp/tomo0611/danime/network/DAnimeApiService.kt
@@ -1,16 +1,28 @@
 package jp.tomo0611.danime.network
 
+import jp.tomo0611.danime.model.GetEpisodesRequest
+import jp.tomo0611.danime.model.GetEpisodesResponse
 import jp.tomo0611.danime.model.GetRecentlyAiredEpisodesResponse
+import jp.tomo0611.danime.model.GetTitleDetailRequest
+import jp.tomo0611.danime.model.GetTitleDetailResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface DAnimeApiService {
 
-    /*@Headers("Content-Type: application/json", "User-Agent: DOCOMO/2.0 Pixel 8(ASTB 2.0;44.00.10203;Android;14;34);locale:en-us;imei:00000;display:AP2A.240705.005;buildid:AP2A.240705.005;safeMode:OFF;webview:Chrome/127.0.6533.23")
+    @Headers("Content-Type: application/json", "User-Agent: DOCOMO/2.0 Pixel 8(ASTB 2.0;44.00.10203;Android;14;34);locale:en-us;imei:00000;display:AP2A.240705.005;buildid:AP2A.240705.005;safeMode:OFF;webview:Chrome/127.0.6533.23")
     @POST("v1/WS600305")
     suspend fun getEpisodes(
         @Body body: List<GetEpisodesRequest>
-    ): List<GetEpisodesResponse>*/
+    ): List<GetEpisodesResponse>
+
+    @Headers("Content-Type: application/json", "User-Agent: DOCOMO/2.0 Pixel 8(ASTB 2.0;44.00.10203;Android;14;34);locale:en-us;imei:00000;display:AP2A.240705.005;buildid:AP2A.240705.005;safeMode:OFF;webview:Chrome/127.0.6533.23")
+    @POST("v1/WS600303")
+    suspend fun getTitleDetail(
+        @Body body: List<GetTitleDetailRequest>
+    ): List<GetTitleDetailResponse>
 
     // Original Function Name : getTvProgram
     @Headers("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0")
