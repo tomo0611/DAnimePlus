7299b38503db38cbd15b65d38d1779068954e9ca
tomo0611 <tomo0611@hotmail.com>
2024-07-18 02:54:36 +0900

    PlayerActivityを追加

---
+++ app/src/main/java/jp/tomo0611/danime/ui/player/PlayerActivity.kt
@@ -0,0 +1,21 @@
+package jp.tomo0611.danime.ui.player
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import jp.tomo0611.danime.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_player)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
